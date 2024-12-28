8d16b52e357c1199eecca734ca8924063e9057ad
Max <max.dodchuk@gmail.com>
2024-12-28 19:42:49 +0200

    fix: first loading flash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { redirect } from 'next/navigation';
+import WhatIsLoading from "@/ui/Loading/Loading";
 
 export default function Page() {
   redirect('/ua/');
+
+  return (
+    <WhatIsLoading />
+  );
 }
