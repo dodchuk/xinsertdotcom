04afca20bb45a37100063985b7cd74ce1af970b9
Max <max.dodchuk@gmail.com>
2024-12-28 19:45:56 +0200

    feat: Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import React, { Suspense } from 'react';
 import Bootstrap from '@/ui/Boostrap/Boostrap';
-import Loading from "@/ui/Loading/Loading";
-import BackgroundMouseMoveEffect from "@/ui/Effects/BackgroundMouseMoveEffect";
+import Loading from '@/ui/Loading/Loading';
+import BackgroundMouseMoveEffect from '@/ui/Effects/BackgroundMouseMoveEffect';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // Global Styles
 import '@/ui/GlobalStyles/global.scss';
@@ -14,9 +15,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
     <body>
-      <Suspense fallback={ <Loading/> }>
+      <Suspense fallback={ <Loading /> }>
         <Bootstrap children={ children } />
-        <BackgroundMouseMoveEffect/>
+        <BackgroundMouseMoveEffect />
+        <SpeedInsights />
       </Suspense>
     </body>
     </html>
